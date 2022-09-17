fa8a2710dfa19bb8552fbbaa920b1bc6bc56029d
wuxulong19950206 <1287173754@qq.com>
2022-09-17 22:06:41 +0800

    debug 测试脚本

--- demo.py
+++ demo.py
@@ -1,12 +1,12 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
 import argparse
 import glob
+import imp
 import multiprocessing as mp
 import os
 import time
 import cv2
 import tqdm
-
 from detectron2.config import get_cfg
 from detectron2.data.detection_utils import read_image
 from detectron2.utils.logger import setup_logger
@@ -41,15 +41,17 @@ def get_parser():
     parser = argparse.ArgumentParser(description="Detectron2 demo for builtin models")
     parser.add_argument(
         "--config-file",
-        default="configs/quick_schedules/mask_rcnn_R_50_FPN_inference_acc_test.yaml",
+        default="configs/CenterNet2_R50_1x.yaml",
         metavar="FILE",
         help="path to config file",
     )
     parser.add_argument("--webcam", action="store_true", help="Take inputs from webcam.")
     parser.add_argument("--video-input", help="Path to video file.")
-    parser.add_argument("--input", nargs="+", help="A list of space separated input images")
+    parser.add_argument("--input",default=["test_images/"], 
+                         nargs="+", help="A list of space separated input images")
     parser.add_argument(
         "--output",
+        default="test_images/output",
         help="A file or directory to save output visualizations. "
         "If not given, will show output in an OpenCV window.",
     )
@@ -63,7 +65,7 @@ def get_parser():
     parser.add_argument(
         "--opts",
         help="Modify config options using the command-line 'KEY VALUE' pairs",
-        default=[],
+        default=["MODEL.WEIGHTS","models/CenterNet2_R50_1x.pth"],
         nargs=argparse.REMAINDER,
     )
     return parser
